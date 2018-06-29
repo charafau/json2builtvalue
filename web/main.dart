23fa1f66fdd8e144d0415e08c2221416a76a7509
charafau <gorudonu@gmail.com>
2018-06-29 02:46:03 +0100

    fixed main code

--- web/main.dart
+++ web/main.dart
@@ -8,16 +8,19 @@ void main() {
 
   querySelector('#convert').onClick.forEach((MouseEvent event) async {
     try {
-//      String json = querySelector('#input_text').text;
+      TextInputElement rootClassNameElement =
+          document.getElementById('root_class_name');
+      String rootClassName = rootClassNameElement.value;
 
       TextAreaElement elementById = document.getElementById('input_text');
       String json = elementById.value;
       print('json is $json');
       Parser parser = new Parser();
-     String outputClasses = parser.parse(json);
+      String outputClasses =
+          parser.parse(json, rootClassName.replaceAll(' ', ''));
 
-     outputText.text = outputClasses;
-      // outputText.text = json;
+      outputText.text = outputClasses;
+      // outputText.text = rootClassName;
     } catch (e) {
       outputText.text = 'Error: ${e.toString()}';
     }
